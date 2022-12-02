223afb34072b2d01b7b3db3912759f8b05b98d02
Anton Dyakov <dyakant@gmail.com>
2022-12-02 12:45:22 +0300

    Initialize spring project

---
+++ src/main/java/com/gitant/sfdservicestation/SfdServiceStationApplication.java
@@ -0,0 +1,13 @@
+package com.gitant.sfdservicestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfdServiceStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfdServiceStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitant/sfdservicestation/SfdServiceStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitant.sfdservicestation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfdServiceStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
