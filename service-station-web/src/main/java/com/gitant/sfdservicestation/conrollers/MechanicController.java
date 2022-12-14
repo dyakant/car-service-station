a26582e6de468724431b2c3d21a22e5a4978087c
Anton Dyakov <dyakant@gmail.com>
2022-12-14 17:47:41 +0300

    Add List of Mechanics on the index page. Closes #17

--- service-station-web/src/main/java/com/gitant/sfdservicestation/conrollers/MechanicController.java
+++ service-station-web/src/main/java/com/gitant/sfdservicestation/conrollers/MechanicController.java
@@ -1,6 +1,8 @@
 package com.gitant.sfdservicestation.conrollers;
 
+import com.gitant.sfdservicestation.services.MechanicService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class MechanicController {
-    @RequestMapping({"/mechanic/index", "/mechanic", "mechanic"})
-    public String listMechanics() {
+
+    private final MechanicService mechanicService;
+
+    public MechanicController(MechanicService mechanicService) {
+        this.mechanicService = mechanicService;
+    }
+
+    @RequestMapping({"/mechanics/index", "/mechanics", "mechanics"})
+    public String listMechanics(Model model) {
+        model.addAttribute("mechanics", mechanicService.findAll());
         return "mechanics/index";
     }
 }
