4777bc8d366a34d90f4d6c14008e87fcb45be018
Anton Dyakov <dyakant@gmail.com>
2022-12-18 23:39:26 +0300

    Create Specialty Map Service Impl. Closes #33

---
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.gitant.sfdservicestation.services;
+
+import com.gitant.sfdservicestation.model.Speciality;
+
+/**
+ * Created by Anton Dyakov on 18.12.2022
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/services/map/SpecialityMapService.java
@@ -0,0 +1,40 @@
+package com.gitant.sfdservicestation.services.map;
+
+import com.gitant.sfdservicestation.model.Speciality;
+import com.gitant.sfdservicestation.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Anton Dyakov on 18.12.2022
+ */
+@Service
+public class SpecialityMapService
+        extends AbstractMapService<Speciality, Long>
+        implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
