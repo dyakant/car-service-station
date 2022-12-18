705315011a7e1bc89e1dc61aee6c3dbd9add8111
Anton Dyakov <dyakant@gmail.com>
2022-12-18 21:30:51 +0300

    Create Mechanic Speciallity Entity, Associate to Mechanic. Closes #29

--- service-station-data/src/main/java/com/gitant/sfdservicestation/model/Mechanic.java
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/model/Mechanic.java
@@ -1,11 +1,23 @@
 package com.gitant.sfdservicestation.model;
 
+import java.util.Set;
+
 /**
  * Created by Anton Dyakov on 08.12.2022
  */
 public class Mechanic extends Person {
+    private Set<Speciality> specialities;
+
     public Mechanic(String name, String surname) {
         this.setFirstName(name);
         this.setLastName(surname);
+    }
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }
---
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/model/Speciality.java
@@ -0,0 +1,16 @@
+package com.gitant.sfdservicestation.model;
+
+/**
+ * Created by Anton Dyakov on 18.12.2022
+ */
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
