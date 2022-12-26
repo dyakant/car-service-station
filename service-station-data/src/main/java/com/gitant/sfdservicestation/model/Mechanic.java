7c1da721f3895017e44baa411ed2f13db2f9db4f
Anton Dyakov <dyakant@gmail.com>
2022-12-27 01:12:25 +0300

    Convert Mechnics to JPA Entities. Closes #39

--- service-station-data/src/main/java/com/gitant/sfdservicestation/model/Mechanic.java
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/model/Mechanic.java
@@ -1,13 +1,24 @@
 package com.gitant.sfdservicestation.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by Anton Dyakov on 08.12.2022
  */
+@Entity
+@Table(name = "mechanics")
 public class Mechanic extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "mechanics_specialities",
+            joinColumns = @JoinColumn(name = "mechanic_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
+
+    public Mechanic() {
+    }
 
     public Mechanic(String name, String surname) {
         this.setFirstName(name);
--- service-station-data/src/main/java/com/gitant/sfdservicestation/model/Speciality.java
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/model/Speciality.java
@@ -1,10 +1,21 @@
 package com.gitant.sfdservicestation.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 /**
  * Created by Anton Dyakov on 18.12.2022
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
+
+    public Speciality() {
+
+    }
 
     public Speciality(String description) {
         this.description = description;
