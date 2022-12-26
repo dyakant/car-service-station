0acf14d9f28f4b83e53144ca91963f1b679cfd0d
Anton Dyakov <dyakant@gmail.com>
2022-12-27 00:45:14 +0300

    Implement Base Entity (JPA). Closes #37

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## About
 
 It is a pet-project to study different staff regarding Spring, Java, Kafka.  
-It is based on Spring Pet-Clinic project and lectures from 
+It is based on Spring Pet-Clinic project and lectures by John Thompson (GuRu) 
 
--- service-station-data/src/main/java/com/gitant/sfdservicestation/model/BaseEntity.java
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/model/BaseEntity.java
@@ -1,11 +1,19 @@
 package com.gitant.sfdservicestation.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
 /**
  * Created by Anton Dyakov on 09.12.2022
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
