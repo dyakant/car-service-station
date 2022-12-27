3db69f764316a2f2bd19255be76777f275241923
Anton Dyakov <dyakant@gmail.com>
2022-12-27 16:01:17 +0300

    Create Visit Map Service. Closes #48

---
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/services/VisitService.java
@@ -0,0 +1,9 @@
+package com.gitant.sfdservicestation.services;
+
+import com.gitant.sfdservicestation.model.Visit;
+
+/**
+ * Created by Anton Dyakov on 27.12.2022
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ service-station-data/src/main/java/com/gitant/sfdservicestation/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package com.gitant.sfdservicestation.services.map;
+
+import com.gitant.sfdservicestation.model.Visit;
+import com.gitant.sfdservicestation.services.VisitService;
+
+import java.util.Set;
+
+/**
+ * Created by Anton Dyakov on 27.12.2022
+ */
+public class VisitMapService
+        extends AbstractMapService<Visit, Long>
+        implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object == null || object.getCar() == null || object.getCar().getId() == null
+                || object.getCar().getOwner() == null || object.getCar().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
